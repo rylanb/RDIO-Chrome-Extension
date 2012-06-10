2be90024d4a2fc628d3df931d628d6b2f92a5126
Rylan Bowers <rylanb@gmail.com>
2012-06-10 07:09:14 -0400

    Updated for new RDIO

--- rdio_ext.js
+++ rdio_ext.js
@@ -21,7 +21,6 @@
       //Check for text changes
       setTimeout(RDIO.helpers.pollChange, 10e3);
       RDIO.bindEvents();
-      var testPerm = "YES";
     },/* End init */
     bindEvents: function bindEvents(){
       /* bind a keypress */
@@ -67,16 +66,16 @@
         RDIO.helpers.animAddClass( $('#track_album') );
         RDIO.helpers.animAddClass( $('#track_name') );
 
-        $('body').css('overflow', 'hidden');
-        globalTimer = setTimeout(function(){RDIO.helpers.playerFadeoutCheck();}, 4e3);
+        // $('body').css('overflow', 'hidden');
+        //globalTimer = setTimeout(function(){RDIO.helpers.playerFadeoutCheck();}, 4e3);
 
-        $('body').bind('mousemove', function(){
-          RDIO.helpers.playerFadeoutTimeOut();
-        });
+        // $('body').bind('mousemove', function(){
+        //   RDIO.helpers.playerFadeoutTimeOut();
+        // });
       },
       playerFadeoutTimeOut: function(){
-        clearTimeout(globalTimer);
-        globalTimer = setTimeout(function(){RDIO.helpers.playerFadeoutCheck();}, 4e3);
+        //clearTimeout(globalTimer);
+        //globalTimer = setTimeout(function(){RDIO.helpers.playerFadeoutCheck();}, 4e3);
         if($('#player_container:animated').length == 0){
           $('#player_container').fadeIn("slow");
           $('#lg_img').css('opacity', '.9');
@@ -95,15 +94,19 @@
       },
       songTextSetup: function(){
         RDIO.helpers.removeDivClasses();
-        $('#track_name').text($('#playerNowPlayingTitle a').text());
-        $('#track_artist').text($('#playerNowPlayingArtist a').text());
-        $('#track_album').text($('#playerNowPlayingAlbum a').text());
+        var $player = $('.player');
+        var $album_link = $player.find('.album_link');
+        var album = decodeURI($album_link.prop('href').split('/album/')[1]).replace(/\_/g, " ");
+
+        $('#track_name').text( $player.find('.name a').text() );
+        $('#track_artist').text( $player.find('.artist a').text() );
+        $('#track_album').text(album);
         RDIO.helpers.animAddClass( $('#track_album') );
         RDIO.helpers.animAddClass( $('#track_name') );
 
         //Get the big image, if possible
-        var img_src = $('#playerNowPlayingImage').attr('src').split('?m=')[1].replace('-200', '-600');
-        img_src = "url(http://media.rd.io/" + img_src.split('&w')[0] + ")";
+        var img_src = $album_link.find('img').attr('src').replace('-200', '-600');
+        img_src = "url(" + img_src + ")";
         if( $('#lg_img').css('background-image') !== img_src ) {
           $('#lg_img').css('background-image', img_src );
         }
@@ -116,10 +119,12 @@
         //obj.css("left", Math.floor(Math.random() * $('body').width)).css("top", Math.floor(Math.random() * $('body').height));
       },
       pollChange: function(){
-        var orig_text_setup = $('#playerNowPlayingTitle a').text();
+        var $player = $('.player');
+        var orig_text_setup = $player.find('.name a').text();
 
         pollit = function(orig_text){
-          var new_text = $('#playerNowPlayingTitle a').text();
+          var $player = $('.player');
+          var new_text = $player.find('.name a').text();
           if(new_text.length > 0){
             if(orig_text !== new_text){
               RDIO.helpers.songTextSetup();
@@ -136,10 +141,9 @@
         $('#full_screen_back').slideUp("fast");
         $(document.body).append("<div class='notification'>Shift-8 to bring that back!</div>");
         $('.notification').delay(3000).fadeOut();
-        $('body').css('overflow', 'auto');
-        $('body').unbind('mousemove');
+        //$('body').css('overflow', 'auto');
+        //$('body').unbind('mousemove');
         clearTimeout(globalTimer);
-        $('#player_container').fadeIn("slow");
         RDIO.helpers.removeDivClasses();
       },
       removeDivClasses: function(){
